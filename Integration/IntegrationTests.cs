152a7dd2c8fac6f6da1c0e0a0daa1c78a230beec
Azamat <abormokov@gmail.com>
2023-06-12 21:22:10 +0600

    ddasdas

--- Integration/IntegrationTests.cs
+++ Integration/IntegrationTests.cs
@@ -1,29 +1,47 @@
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Entities.Models;
 
 namespace Integration;
 
 public class IntegrationTests: IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
-    public async Task GetAllOwners_ReturnsAListOfOwners()
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public IntegrationTests(WebApplicationFactory<Program> factory)
     {
-        // Arrange
-        var server = new TestServer(new WebHostBuilder()
-            .UseConfiguration(new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build())
-            .UseStartup<Startup>());
-        var client = server.CreateClient();
+        _factory = factory;
+    }
+    [Fact]
+    public async Task GetAllOwners_ReturnsOkResponse()
+    {
+        //Arrange
+        var client = _factory.CreateClient();
 
         // Act
         var response = await client.GetAsync("/api/owner");
         response.EnsureSuccessStatusCode();
-        
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+    [Fact]
+    public async Task GetAllOwners_ReturnsAListOfOwners()
+    {
+        //Arrange
+        var client = _factory.CreateClient();
+        // Act
+        var response = await client.GetAsync("/api/owner");
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        var owners = JsonConvert.DeserializeObject<List<Owner>>(responseString);
+        // Assert
+        Assert.NotEmpty(owners);
     }
 }
